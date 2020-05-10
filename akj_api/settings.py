ead73c76b2944df0d7f1d27c837567bafe9262a5
hardeep980 <hardeep0khalsa122@gmail.com>
2020-05-10 14:17:03 +0530

    heroku configs

--- akj_api/settings.py
+++ akj_api/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,3 +112,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
