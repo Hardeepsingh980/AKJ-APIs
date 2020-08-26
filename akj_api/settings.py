67af10b09c6578f162d4a884cccbf2079f326244
Hardeep Singh <hardeep0khalsa122@gmail.com>
2020-08-26 16:09:19 +0530

    changed database

--- akj_api/settings.py
+++ akj_api/settings.py
@@ -67,10 +67,21 @@ WSGI_APPLICATION = 'akj_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vldtbgou',
+        'USER': 'vldtbgou',
+        'PASSWORD': 'L6m0BQsx-pXm2U-EX9uRTopnQkPnUycG',
+        'HOST': 'lallah.db.elephantsql.com',
+        'PORT': 5432,
     }
 }
 
